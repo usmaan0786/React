da6602966e37039a587b698c9de78106e775c039
Usman Manzoor <72275107+UsmanManzoor111@users.noreply.github.com>
2023-04-28 17:57:22 +0500

    Add files via upload

---
+++ state_in_react/src/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+var isDone = false;
+
+function strike() {
+  document.getElementById("root").style.textDecoration = "line-through";
+}
+
+function unStrike() {
+    document.getElementById("root").style.textDecoration = null;
+}
+
+function App() {
+  return (
+    <div>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
+  );
+}
+
+export default App;
